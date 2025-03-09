e8ef846d6b35f8ec9a473d8c57665752874f097d
Harsha <harshagowtham30@gmail.com>
2025-03-09 11:07:05 +0530

    layout issue fix

--- Frontend/inventory_management_Fe/src/app/app.config.ts
+++ Frontend/inventory_management_Fe/src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideStoreDevtools } from '@ngrx/store-devtools'; // ✅ Import Redux DevTools
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
+import { authReducer } from './store/auth/auth.reducer';
+import { AuthEffects } from './store/auth/auth.effects';
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(appRoutes, withComponentInputBinding()), 
+  provideStore({ auth: authReducer }),
+  provideEffects([AuthEffects]),
+  provideCharts(withDefaultRegisterables()),
+  provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode(), }) 
+  ]
 };
--- Frontend/inventory_management_Fe/src/main.ts
+++ Frontend/inventory_management_Fe/src/main.ts
@@ -1,22 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideStore } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/app.routes';
-import { authReducer } from './app/store/auth/auth.reducer';
-import { AuthEffects } from './app/store/auth/auth.effects'; // ✅ Import AuthEffects
-import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
-import { provideStoreDevtools } from '@ngrx/store-devtools'; // ✅ Import Redux DevTools
-import { isDevMode } from '@angular/core';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(appRoutes, withComponentInputBinding()), // ✅ Enables component input bindings
-    provideStore({ auth: authReducer }),
-    provideEffects([AuthEffects]), // ✅ Add AuthEffects to handle login/register
-    provideCharts(withDefaultRegisterables()),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode(),}) // ✅ Enable Redux DevTools
-
-  ],
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
